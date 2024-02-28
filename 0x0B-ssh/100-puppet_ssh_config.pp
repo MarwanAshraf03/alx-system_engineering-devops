ffae9495b0d07dde82058896e05e4d07fd2e398d
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-02-28 15:39:46 +0200

    update

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++
@@ -1,9 +0,0 @@
-# a comment
-class ssh {
-  options => {
-    'User' => 'ubuntu',
-    'Hostname' => '54.172.106.250',
-    'HostKey' => '~/.ssh/school',
-    'PasswordAuthentication' => 'no'
-  }
-}
