cdda574c94e6ceeebc0278a07fe55997a582e83a
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-02-28 22:10:59 +0200

    update

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,11 @@
 # a comment
-file_line {:
+file_line {'set password authentication to no':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no',
 }
 
-file_line {:
+file_line { 'use school identity file':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '    IdentityFile ~/.ssh/school',
