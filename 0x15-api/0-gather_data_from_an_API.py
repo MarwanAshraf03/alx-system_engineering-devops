5da113d64a828c0db5ba20e8a2d1971ea54e6097
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-03-26 02:26:30 +0200

    update

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,13 +6,13 @@ from sys import argv
 if __name__ == "__main__":
     link = f"https://jsonplaceholder.typicode.com/users/{argv[1]}"
     user_data = json.loads(requests.get(link).content.decode('utf8'))
-    todos_data = json.loads(requests.get(f"{link}/todos").content.decode('utf8'))
-    total = len(todos_data)
+    toddata = json.loads(requests.get(f"{link}/todos").content.decode('utf8'))
+    total = len(toddata)
     done = 0
-    for i in todos_data:
+    for i in toddata:
         if i['completed']:
             done += 1
     print(f"Employee {user_data['name']} is done with tasks({done}/{total})")
-    for i in todos_data:
+    for i in toddata:
         if i['completed']:
-            print(f"\t{i['title']}")
+            print(f"\t {i['title']}")
