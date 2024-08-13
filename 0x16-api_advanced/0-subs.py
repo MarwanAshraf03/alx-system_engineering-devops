be0cbd01c75b2a9aafc3add3f36b5ae6520d4c18
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-08-14 01:58:50 +0300

    0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import requests
+import json
+"""
+0-main
+"""
+def number_of_subscribers(subreddit):
+    resp = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json")
+    if resp.status_code != 200:
+        return (0)
+    return (json.loads(resp.text)['data']['subscribers'])
