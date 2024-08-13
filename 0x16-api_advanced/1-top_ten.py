28892ebad1e715b8dda9503348c674bebe5546b6
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-08-14 02:37:47 +0300

    1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+prints the number of subscribers to a subreddit
+"""
+import json
+import requests
+
+
+def top_ten(subreddit):
+    """
+    prints the number of subscribers to a subreddit
+    """
+    resp = requests.get(
+        f"https://www.reddit.com/r/{subreddit}/hot.json?limit=9")
+    if resp.status_code != 200:
+        return (0)
+    count = 1
+    for i in json.loads(resp.text)['data']['children']:
+        print(count, i['data']['title'])
+        count += 1
