52d528a36adf0cab263eafdc6c9a42ebb97422fd
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-08-14 02:13:58 +0300

    0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,8 @@ def number_of_subscribers(subreddit):
     """
     prints the number of subscribers to a subreddit
     """
-    resp = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json")
+    h = {'User-agent': 'Unix:0-subs:v1'}
+    resp = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json", headers=h)
     if resp.status_code != 200:
         return (0)
     return (json.loads(resp.text)['data']['subscribers'])
