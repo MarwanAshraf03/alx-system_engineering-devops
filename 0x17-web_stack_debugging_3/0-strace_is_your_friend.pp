8e087b13c58d026b8207cd756b482f7815dfc1ad
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-08-15 03:45:04 +0300

    0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,8 @@
+# This Puppet manifest creates the /var/www/html/index.html file with "hello world" as its content.
+
 file { '/var/www/html/index.html':
   ensure  => file,
-  content => 'hello world',
+  content => "hello world\n",
   owner   => 'root',
   group   => 'root',
   mode    => '0644',
